2e49f868bf7bb57111f529bc3666b7e4a442fe6b
Logan Hallee <lhallee@udel.edu>
2025-10-28 17:15:27 -0400

    Update main.py

--- src/protify/main.py
+++ src/protify/main.py
@@ -28,7 +28,7 @@ def parse_arguments():
     parser.add_argument("--plots_dir", default="plots", help="Directory to save plots.")
     parser.add_argument("--replay_path", type=str, default=None, help="Path to the replay file.")
     parser.add_argument("--pretrained_probe_path", type=str, default=None) # TODO not used right now
-
+    
     # ----------------- DataArguments ----------------- #
     parser.add_argument("--delimiter", default=",", help="Delimiter for data.")
     parser.add_argument("--col_names", nargs="+", default=["seqs", "labels"], help="Column names.") # DEPRECATED, found automatically now
